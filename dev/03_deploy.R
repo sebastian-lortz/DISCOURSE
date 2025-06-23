057690468416a7e73766d99555912efde500c07a
sebastian-lortz <s.a.j.lortz@gmail.com>
2025-06-23 13:16:18 +0200

    update

--- dev/03_deploy.R
+++ dev/03_deploy.R
@@ -50,7 +50,7 @@ rsconnect::deployApp(
     # Add any additional files unique to your app here.
     "R/",
     "inst/",
-    "data/",
+    "data-raw/",
     "NAMESPACE",
     "DESCRIPTION",
     "app.R"
